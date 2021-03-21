ee150cf3dad2bc8571a47f4bfa4c52b3cece0367
Artur <aepremyan3993@gmail.com>
2021-03-22 00:27:05 +0300

    fdkl

--- epremyan/settings.py
+++ epremyan/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,9 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -142,3 +140,7 @@ EMAIL_HOST_USER = "aepremyan3993@gmail.com"
 EMAIL_HOST_PASSWORD = "12torrentino"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
